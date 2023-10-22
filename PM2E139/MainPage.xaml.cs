bfe33b90a5927e2b0453949f1167a846f7305f28
Josue Meraz <josuemeraz7@gmail.com>
2023-10-22 16:06:08 -0600

    Agregar archivos de proyecto.

---
+++ PM2E139/MainPage.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Plugin.Media;
+using PM2E139.Services;
+using PM2E139.Views;
+using Plugin.Geolocator;
+using Xamarin.Forms.Maps;
+using Plugin.Media.Abstractions;
+using Plugin.Geolocator.Abstractions;
+using static Xamarin.Essentials.Permissions;
+
+namespace PM2E139
+{
+    public partial class MainPage : ContentPage
+    {
+        String pathImagen;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ubicacion();
+
+        }
+
+        //protected override async void OnAppearing()
+        //{
+        //    base.OnAppearing();
+        //    var conexion = Connectivity.NetworkAccess;
+        //    var local = CrossGeolocator.Current;
+
+        //    if (conexion == NetworkAccess.Internet)
+        //    {
+        //        if (local != null)
+        //        {
+        //            local.PositionChanged += Local_PositionChanged; ;
+        //            if (!local.IsListening)
+        //            {
+        //                await local.StartListeningAsync(TimeSpan.FromSeconds(10), 100);
+        //            }
+
+        //            var posicion = await local.GetPositionAsync();
+        //            txtLat.Text = posicion.Latitude.ToString();
+        //            txtLon.Text = posicion.Longitude.ToString();
+        //        }
+        //        else
+        //        {
+        //            var posicion = await local.GetLastKnownLocationAsync();
+        //            txtLat.Text = posicion.Latitude.ToString();
+        //            txtLon.Text = posicion.Longitude.ToString();
+        //        }
+        //    }
+        //}
+
+        private void salir_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        private async void Agregar_Clicked(object sender, EventArgs e)
+        {
+            if (pathImagen == "" || String.IsNullOrEmpty(txtDes.Text) || String.IsNullOrEmpty(txtLat.Text) || String.IsNullOrEmpty(txtLon.Text))
+            {
+                await DisplayAlert("Aviso", "Datos obligatorios: imagen, latitud, longitud, descripcion", "Ok");
+
+            }
+            else
+            {
+                var emple = new Lugar
+                {
+                    latitud = txtLat.Text,
+                    longitud = txtLon.Text,
+                    descripcion = txtDes.Text,
+                    image = pathImagen
+                };
+
+                var resultado = await App.BaseDatos.EmpleadoGuardar(emple);
+                if (resultado != 0)
+                {
+                    await DisplayAlert("Aviso", "Datos guardados con exito", "OK");
+                    pinkFoto.Source = ("");
+                    pathImagen = "";
+                    txtDes.Text = "";
+                    txtLat.Text = "";
+                    txtLon.Text = "";
+                    ubicacion();
+
+                }
+                else
+                {
+                    await DisplayAlert("Error", "Debe habilitar la ubicacion", "OK");
+                }
+            }
+             ubicacion();
+        }
+
+        private async void Lista_Clicked(object sender, EventArgs e)
+        {
+            var newpage = new ListEmple();
+            await Navigation.PushAsync(newpage);
+
+        }
+
+        private async void pinkFoto_Clicked(object sender, EventArgs e)
+        {
+            var takepic = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "PhotoApp",
+                Name = "photo.jpg"
+            });
+            pathImagen = takepic.Path;
+
+
+            if (takepic != null)
+            {
+                pinkFoto.Source = ImageSource.FromStream(() => {
+                    return takepic.GetStream();
+                });
+            }
+        }
+
+        async private void ubicacion()
+        {
+            try
+            {
+                var local = CrossGeolocator.Current;
+                local.DesiredAccuracy = 50;
+                if (local.IsGeolocationAvailable)
+                {
+                    if (local.IsGeolocationEnabled)
+                    {
+                        if (!local.IsListening)
+                        {
+                            await local.StartListeningAsync(TimeSpan.FromSeconds(1),5);
+                        }
+                        local.PositionChanged += Local_PositionChanged1;
+                    }
+                }
+
+                //var posicion = await local.GetPositionAsync();
+
+                //if (local != null)
+                //{
+                //    txtLat.Text = posicion.Latitude.ToString();
+                //    txtLon.Text = posicion.Longitude.ToString();
+                //}
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Error", "Error: " + fnsEx, "OK");
+            }
+
+        }
+
+        private void Local_PositionChanged1(object sender, PositionEventArgs e)
+        {
+            
+            txtLat.Text = e.Position.Latitude.ToString();
+            txtLon.Text = e.Position.Longitude.ToString();
+
+        }
+
+        public bool EmptyField(Entry campo)
+        {
+            return String.IsNullOrEmpty(campo.Text);
+        }
+    }
+}
---
+++ PM2E139/Views/VerRegistro.xaml.cs
@@ -0,0 +1,78 @@
+﻿using PM2E139.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2E139.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VerRegistro : ContentPage
+    {
+        String longitud, latitud;
+        String pathImagen;
+
+        private async void Lista_Clicked(object sender, EventArgs e)
+        {
+            var newpage = new ListEmple();
+            await Navigation.PushAsync(newpage);
+        }
+
+        private async void Eliminar_Clicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(lblCod.Text))
+            {
+                await DisplayAlert("Aviso", "No se puede actualizar si esta no es una vista", "Ok");
+            }
+            else
+            {
+                var emple = new Lugar
+                {
+                    id = Convert.ToInt32(lblCod.Text)
+                };
+
+                var resultado = await App.BaseDatos.EmpleadoBorrar(emple);
+                if (resultado != 0)
+                {
+                    await DisplayAlert("Aviso", "Dato eliminado con exito", "Ok");
+                    await Navigation.PopAsync();
+                }
+                else
+                {
+                    await DisplayAlert("Error", "Error al eliminar el dato", "Ok");
+                }
+
+            }
+        }
+
+        private async void Enviar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                pathImagen = lblimage.Text;
+
+                var sharephoto = pathImagen;
+                await Share.RequestAsync(new ShareFileRequest
+                {
+                    Title = "Foto",
+                    File = new ShareFile(sharephoto)
+                });
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.ToString(), "Ok");
+            }
+        }
+
+        public VerRegistro()
+        {
+            InitializeComponent();
+            longitud = txtLon.Text;
+            latitud = txtLat.Text;
+        }
+    }
+}
